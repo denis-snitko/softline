27685793f1672692304bd6557c9fada79557f7fe
Denis Snitko <denissnitko@gmail.com>
2020-06-11 21:37:38 +0300

    Update

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,2 +1,33 @@
-@@include('../modules/header/_header.js');
+$(document).ready(function () {
+
+
+    // Search show
+
+    let $searchInput = $('.form__input');
+    let $searchResult = $('.search__result');
+
+    $searchInput.focusin(function () {
+        $searchResult.show(150);
+    });
+
+    $searchInput.focusout(function () {
+        $searchResult.hide(150);
+    });
+
+
+    // Main tabs
+
+    $('.main-tabs__title-item').click(function () {
+        let id = $(this).attr('data-tab'),
+            content = $('.main-tabs__content-item[data-tab="' + id + '"]');
+
+        $('.main-tabs__title-item.main-tabs--active').removeClass('main-tabs--active');
+        $(this).addClass('main-tabs--active');
+
+        $('.main-tabs__content-item').removeClass('active');
+        content.addClass('active');
+    });
+})
+
+
 
